f1bf22e64b838333fb1d8210c28e6f56ce8d1005
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-03-18 20:54:22 -0600

    created supabase

--- lib/supabaseclient.ts
+++ lib/supabaseclient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://YOUR-SUPABASE-URL.supabase.co";
-const SUPABASE_KEY = "YOUR-ANON-KEY";
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_KEY = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
