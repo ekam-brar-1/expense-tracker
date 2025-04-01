1e735072545994574466691aa66b96bd63b16874
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-04-01 16:44:23 -0600

    oops moment (pushed api key, had to regen it)

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ export default ({ config }) => {
     extra: {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-      "GEMINI_API_KEY": "AIzaSyCTcsYZztGzk9xn_pvNUbQVZA7_AqGDbsQ",
+      GEMINI_API_KEY : "AIzaSyD9URTuj02n70e99YqcTld4G4xEuINM93E",
     },
   };
 };
